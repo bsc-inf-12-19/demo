a3fc74589ed30a35b309101356516a3063c7a4ac
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-01-10 15:45:29 -0800

    few files added

---
+++ src/main/java/com/example/demo/tourism/TourismConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.tourism;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class TourismConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            TourismRepository repository){
+        return args -> {
+            Tourism david = new Tourism(
+                    "david@gmail.com",
+                    "david"
+            );
+            Tourism king = new Tourism(
+                    "king@gmail.com",
+                    "king"
+            );
+            repository.saveAll(
+                    List.of(david, king)
+            );
+        };
+    }
+}
